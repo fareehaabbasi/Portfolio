a721cf3d94c26a48e4a162187589ef5cc3b143af
fareehaabbasi <abbasifareeha9@gmail.com>
2026-09-04 22:53:21 +0500

    add-new

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -27,91 +27,142 @@ const HeroSec = (darkMode, toggleDarkMode) => {
 
   return (
     <section className="min-h-screen relative overflow-hidden max-w-7xl mx-auto px-6 md:px-20">
-      {/* Name title */}
-      <div className="flex flex-col md:flex-row justify-center md:justify-between md:pt-20 pt-20">
-        <h5 className="font-bold md:text-left">
-          HI, I'm <span className="text-orange-500">FAREEHA</span>
-        </h5>
-      </div>
+     {/* FRONTEND + DEVELOPER */}
+<div
+  className="
+    absolute
+    top-28
+    md:top-34
+    left-1/2
+    -translate-x-1/2
+    z-10
+    w-max
+  "
+>
+  {/* HI, I'M FAREEHA */}
+  <div
+    className="
+      absolute
+      left-0
+      top-[-1.4rem]
+      sm:top-[-2rem]
+      md:top-[-2rem]
+      lg:top-[-3rem]
 
-      {/* Huge Text */}
-      <h1
-        className={`absolute
-    inset-x-0
-    top-24
-    text-center
-    font-frontend
-    text-[clamp(5rem,20vw,24rem)]
-    leading-[0.8]
-    tracking-widest
-    text-[#F5E6D3]
-    whitespace-nowrap
-    top-27
-    md:top-28
-    uppercase 
-    `}
-      >
-        Frontend
-      </h1>
+      font-bold
+      text-xs
+      sm:text-sm
+      md:text-base
+      lg:text-lg
 
-      {/* Name title */}
-      <div className="flex absolute left-1/2 -translate-x-1/2 md:left-auto md:translate-x-0 md:right-[5%] top-18 md:top-18 z-50 text-center md:flex-row md:px-10 lg:pt-55 md:pt-36 sm:pt-36 pt-23">
-        <div className="bg-black">
-          <h5 className="md:text-end text-center text-2xl md:text-4xl font-frontend">
-            <span className="text-orange-500">DEVELOPER</span>
-          </h5>
-        </div>
-      </div>
+      whitespace-nowrap
+    "
+  >
+    HI, I'M <span className="text-orange-500">FAREEHA</span>
+  </div>
 
-      {/* Hero Image */}
-      <div
-        className="
-            absolute
-            left-1/2
-            top-[3rem]
-            md:top-[1rem]
-            lg:top-[-1rem]
-            z-20
-            -translate-x-1/2
-            flex
-            justify-center
-            pointer-events-none
-          "
-      >
-        <img
-          src={heroPic}
-          alt="Fareeha - Frontend Developer"
-          className="
-          w-[40vw]
-          max-w-[250px]
-          h-auto
-          object-contain
+  {/* FRONTEND */}
+  <h1
+    className="
+      font-frontend
+      text-[clamp(5rem,20vw,24rem)]
+      leading-[0.8]
+      tracking-widest
+      text-[#F5E6D3]
+      whitespace-nowrap
+      uppercase
+    "
+  >
+    Frontend
+  </h1>
 
-          sm:w-[30vw]
-          sm:max-w-[380px]
+  {/* DEVELOPER */}
+  <div
+    className="
+      absolute
+      right-0
+      bottom-[-0.02em]
+      translate-x-[5%]
+      bg-black
+      z-30
+    "
+  >
+    <h5
+      className="
+        font-frontend
+        text-orange-500
+        font-bold
+        leading-none
+        text-[clamp(1.5rem,4vw,4rem)]
+        whitespace-nowrap
+      "
+    >
+      DEVELOPER
+    </h5>
+  </div>
+</div>
 
-          md:w-[30vw]
-          md:max-w-[520px]
+    {/* HERO IMAGE */}
+<div
+  className="
+    absolute
+    left-1/2
+    -translate-x-1/2
 
-          lg:w-[30vw]
-          lg:max-w-[700px]
+    top-[2.5rem]
+    sm:top-[-0.2rem]
+    md:top-[1rem]
+    lg:top-[-2rem]
+    xl:top-[-3rem]
 
-          xl:w-[48vw]
-          xl:max-w-[800px]
-        "
-        />
-      </div>
+    z-20
+    flex
+    justify-center
+    pointer-events-none
+  "
+>
+  <img
+    src={heroPic}
+    alt="Fareeha - Frontend Developer"
+    className="
+      h-auto
+      object-contain
+
+      w-[42vw]
+      max-w-[220px]
+
+      sm:w-[40vw]
+      sm:max-w-[290px]
+
+      md:w-[38vw]
+      md:max-w-[400px]
+
+      lg:w-[34vw]
+      lg:max-w-[600px]
+
+      xl:w-[38vw]
+      xl:max-w-[700px]
+
+      /* bottom crop */
+      [clip-path:inset(0_0_40px_0)]
+
+      sm:[clip-path:inset(0_0_100px_0)]
+
+      md:[clip-path:inset(0_0_0_0)]
+    "
+  />
+</div>
 
       {/* left text */}
-      <div className="text-center md:text-left flex absolute md:left-16 left-1/2 -translate-x-1/2 md:translate-x-0 top-[240px] sm:top-[250px] md:top-[350px] lg:top-96 z-50 text-orange-100">
+      <div className="text-center sm:text-left flex absolute md:left-16 sm:left-40 left-1/2 -translate-x-1/2 md:translate-x-0 top-[50vw] sm:top-[260px] md:top-[350px] lg:top-96 z-50 text-orange-100">
         {/* Main text */}
         <div>
-          <h5 className="md:w-60 w-80 text-sm md:text-base">
+          <h5 className="sm:w-60 w-80 text-sm md:text-base">
             I BUILD MODERN, FAST & RESPONSIVE WEBSITES THAT HELP BUSINESSES
             GROW.
           </h5>
 
-          <button className="mt-2 w-fit mx-auto md:mx-0 border text-sm rounded-lg border-orange-500 px-5 py-2 text-orange-500 flex items-center gap-3 font-extrabold">
+          <button className="mt-2 w-fit mx-auto sm:mx-0 border text-sm rounded-lg border-orange-500 px-5 py-2 text-orange-500 flex items-center gap-3 font-extrabold">
             View My Work
             <ArrowRight className="text-lg text-orange-500" />
           </button>
@@ -120,15 +171,15 @@ const HeroSec = (darkMode, toggleDarkMode) => {
 
       {/* right text */}
       {/* Mobile only text */}
-        <div className="md:block hidden absolute top-24 left-1/2 -translate-x-1/2 w-60 text-center">
-          <h5 className="text-sm">
-            LET’S TURN IDEAS INTO DIGITAL EXPERIENCES.
-          </h5>
+      <div className="sm:block hidden absolute right-0 -translate-x-24 sm:-translate-x-14 w-60 text-right top-[240px] sm:top-[260px] md:top-[350px] lg:top-96 z-50 text-orange-100">
+        <h5 className="text-sm">
+          LET’S TURN IDEAS INTO DIGITAL EXPERIENCES.
+        </h5>
 
-          <h5 className="mt-3 text-orange-500 font-serif text-xl">
-            FAREEHA ABBASI
-          </h5>
-        </div>
+        <h5 className="mt-3 text-orange-500 font-serif text-xl">
+          FAREEHA ABBASI
+        </h5>
+      </div>
     </section>
   );
 };
